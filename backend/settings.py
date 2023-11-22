1e8e9cf487b77680ff3a65a249899ecfdd29c55b
jverdesoto <verdesoto.diseno@gmail.com>
2023-11-21 21:44:01 -0500

    Added allowed hosts with internal

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-qpdgihvsni8dmz2g%w&_dop0)shn+^^bqu=wa1iw#(cd1oupa$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sei75finalprojectbackend-production.up.railway.app']
+ALLOWED_HOSTS = ['sei75finalprojectbackend-production.up.railway.app', 'sei75finalprojectbackend.railway.internal']
 
 
 # Application definition
