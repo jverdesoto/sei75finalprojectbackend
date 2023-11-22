e971b889946052c79585a9529c7565cdc8cec1e3
jverdesoto <verdesoto.diseno@gmail.com>
2023-11-21 20:08:41 -0500

    For railway

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-qpdgihvsni8dmz2g%w&_dop0)shn+^^bqu=wa1iw#(cd1oupa$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOST = ['*']
 
 
 # Application definition
